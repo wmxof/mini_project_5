8f8e2ad1d4ff023bf2e4b67a93c665e2f1188e95
wmxof <108640363+wmxof@users.noreply.github.com>
2025-12-22 16:07:16 +0900

    Update route.jsx

--- miniproject4-next/src/app/api/signup/route.jsx
+++ miniproject4-next/src/app/api/signup/route.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 // const API_BASE_URL =
 //     process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
 
-const API_BASE_URL = "http://10.99.2.11:8080"
+const API_BASE_URL = "http://13.124.180.223:8080"
 
 export async function POST(request) {
     try {
